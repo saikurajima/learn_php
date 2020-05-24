35c5716e3564bc05bbd1a46efb756525562ecd5f
root <root@DESKTOP-7P4HH0E.localdomain>
2020-05-24 09:34:40 +0800

    POF

---
+++ 05-POF.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$name="mon site web";
+
+$html_code = <<<EOF
+<p>
+<a href="https://kunqian888.cn">$name</a>
+</p>
+EOF;
+
+echo $html_code;
+?>
+
+
+
+</body>
+</html>
