81cc3ab3ff13e9ab9f915c0c184e0d7d935696bc
root <root@DESKTOP-7P4HH0E.localdomain>
2020-05-24 09:27:35 +0800

    变量规则

---
+++ 02-special_variates.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>second</h1>
+<?php
+echo '我永远喜欢樱岛麻衣学姐'
+?>
+
+ <?php
+// print 'no';
+?>
+
+<br>
+<br>
+
+<?php
+$x=5;
+$y=6;
+$z=$x+$y;
+echo $z;
+$status_code=200;
+echo $status_code;
+?>
+
+
+</body>
+</html>
