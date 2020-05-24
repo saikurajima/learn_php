bb616a03c75d95d158558cb8bd31d122dba38e9e
root <root@DESKTOP-7P4HH0E.localdomain>
2020-05-24 09:37:38 +0800

    var_dump函数

---
+++ 07-var_dump.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$name="樱岛麻衣";
+$x=true;
+
+//EOF是分界符，里面所有的语句都不需要转移
+$html_code=<<<EOF
+<p>
+<a href="https://kunqian888.cn">$name</a>
+</p>
+EOF;
+
+var_dump($html_code);
+var_dump($x);
+?>
+
+
+
+
+</body>
+</html>
